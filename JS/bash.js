7bf4d29e17a6e2520498bd529c128f3db8675810
KRISHANU SAINI <56930593+krishanu-2001@users.noreply.github.com>
2020-10-18 19:06:19 +0530

    added python venv bash

--- JS/bash.js
+++ JS/bash.js
@@ -1,6 +1,6 @@
 //blog js file
 var counter = 0
-var MAX = 3-1
+var MAX = 3
 /*
   Fill array using some automation and update MAX as len(Array)
 */
@@ -8,13 +8,63 @@ var MAX = 3-1
 var items = ["\necho hello\n ",
 "\nmkdir orange\n ",
 "\ncd orange\n ",
+`
+@ECHO OFF
+ECHO Congratulations! Your batch file executed successfully.
+IF EXIST "%~dp0\\venv\\Scripts\\python.exe" (
+echo OK
+"%~dp0\\venv\\Scripts\\python.exe" "app.py" 
+) ELSE (
+ ECHO installing pip requirements in venv: python -m venv venv!
+ python -m venv venv
+ "%~dp0\\venv\\Scripts\\pip" install -r requirements.txt
+ "%~dp0\\venv\\Scripts\\python.exe" "app.py" 
+)
+PAUSE
+
+`,
 ]
 
 /* use <br /> here */
 var explain = ["prints hello in command line",
 "makes <b>/orange directory</b> in current directory",
-"change directory to <b>/orange</b>"
+"change directory to <b>/orange</b>",
+`
+Script to start python server
+<br />
+<br />
+<br /> checks if already venv created
+<br />
+<br /> and simply starts the <b>app.py</b> server
+<br />
+<br /> otherwise install python virtual environment 
+<br /> as <b>venv</b> where we will install requirements.txt 
+<br />
+<br /> and now start the app.py server
+`
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $(Document).ready(function(){
 	  counter = 0
@@ -22,10 +72,20 @@ $(Document).ready(function(){
 	  //slides.addEventListener("click", show_menu);
 	  $('#goleft').click(goleft);
 	  $('#goright').click(goright);
+
+
+	  /* manualwork */
+	  $('#itemlist_1').click(setcounter_3);
 });
 
+
+/* please change manual linking */
+function setcounter_3(){
+	counter = 3
+	refreshpage(counter)
+}
+
 function goleft(){
-	var element = document.querySelectorAll(".menud");
     if(counter == 0){
     	alert("can't go back");
     } else {
@@ -35,7 +95,6 @@ function goleft(){
 }
 
 function goright(){
-	var element = document.querySelectorAll(".menud");
     if(counter == MAX){
     	alert("can't go ahead");
     } else {

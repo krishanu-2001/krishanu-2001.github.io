e53736ddaaa5eacee9dc148189f65c3c270690e7
metakrishanu <krishanu.saini@metaarbor.com>
2026-03-28 02:07:30 -0500

    Fix path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/portfolio-research/' : '/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
